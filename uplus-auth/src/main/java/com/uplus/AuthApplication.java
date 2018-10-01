b4d3fc9875002ebd68ef21803deafc86c1866c06
Deament <591518884@qq.com>
2018-10-01 15:57:33 +0800

    no commit message

--- uplus-auth/src/main/java/com/uplus/AuthApplication.java
+++ uplus-auth/src/main/java/com/uplus/AuthApplication.java
@@ -15,27 +15,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class AuthApplication {
 
-    public static void main(
-            String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
-    }
-    // //-----------------下面代码处理初始化一个用户-------------
-    // //用户名:leftso 用户密码:111aaa 用户角色:ROLE_USER
-    // @Autowired
-    // AccountRepository accountRepository;
-    //
-    // @Autowired
-    // public void init(){
-    // try {
-    // Account account=new Account();
-    // account.setName("leftso");
-    // account.setPassword("111aaa");
-    // account.setRoles(new String []{"ROLE_USER"});
-    // accountRepository.deleteAll();
-    // accountRepository.save(account);
-    //
-    // } catch (Exception e) {
-    // e.printStackTrace();
-    // }
-    // }
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
 }
--- uplus-auth/src/main/java/com/uplus/auth/config/AuthorizationServerConfiguration.java
+++ uplus-auth/src/main/java/com/uplus/auth/config/AuthorizationServerConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -23,7 +24,7 @@ import org.springframework.security.oauth2.provider.client.JdbcClientDetailsServ
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 /**
  * 认证授权服务端
@@ -34,122 +35,105 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
-    //
-    // @Value("${resource.id:spring-boot-application}") //
-    // 默认值spring-boot-application
-    // private String resourceId;
+	//
+	// @Value("${resource.id:spring-boot-application}") //
+	// 默认值spring-boot-application
+	// private String resourceId;
 
-    // @Value("${access_token.validity_period:3600}") // 默认值3600
-    // int accessTokenValiditySeconds = 3600;
+	// @Value("${access_token.validity_period:3600}") // 默认值3600
+	// int accessTokenValiditySeconds = 3600;
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private DataSource dataSource;
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	@Autowired
+	RedisConnectionFactory connectionFactory;
 
-    /**
-     * token converter
-     *
-     * @return
-     */
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter() {
-            /***
-             * 重写增强token方法,用于自定义一些token返回的信息
-             */
-            @Override
-            public OAuth2AccessToken enhance(
-                    OAuth2AccessToken accessToken,
-                    OAuth2Authentication authentication) {
-                // String userName = authentication.getUserAuthentication().getName();
-                // 与登录时候放进去的UserDetail实现类一直查看link{SecurityConfiguration}
+	@Autowired
+	private DataSource dataSource;
 
-                // /** 自定义一些token属性 ***/
-                final Map<String, Object> additionalInformation = new HashMap<>();
-                //
-                // additionalInformation.put("user",
-                // authentication.getUserAuthentication().getPrincipal());
-                // additionalInformation.put("userName", userName);
-                // additionalInformation.put("roles", user.getAuthorities());
-                ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
-                OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
-                return enhancedToken;
-            }
+	/**
+	 * token converter
+	 *
+	 * @return
+	 */
+	// @Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter() {
+			/***
+			 * 重写增强token方法,用于自定义一些token返回的信息
+			 */
+			@Override
+			public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+				// String userName = authentication.getUserAuthentication().getName();
+				// 与登录时候放进去的UserDetail实现类一直查看link{SecurityConfiguration}
 
-        };
-        accessTokenConverter.setSigningKey("auth");// 测试用,资源服务使用相同的字符达到一个对称加密的效果,生产时候使用RSA非对称加密方式
-        return accessTokenConverter;
-    }
+				// /** 自定义一些token属性 ***/
+				final Map<String, Object> additionalInformation = new HashMap<>();
+				//
+				// additionalInformation.put("user",
+				// authentication.getUserAuthentication().getPrincipal());
+				// additionalInformation.put("userName", userName);
+				// additionalInformation.put("roles", user.getAuthorities());
+				((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
+				OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
+				return enhancedToken;
+			}
 
-    @Override
-    public void configure(
-            AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(this.authenticationManager);
-        endpoints.accessTokenConverter(accessTokenConverter());
-        endpoints.tokenStore(tokenStore());
+		};
+		accessTokenConverter.setSigningKey("123");// 测试用,资源服务使用相同的字符达到一个对称加密的效果,生产时候使用RSA非对称加密方式
+		return accessTokenConverter;
+	}
 
-    }
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(this.authenticationManager);
+		// endpoints.accessTokenConverter(accessTokenConverter());
+		endpoints.tokenStore(tokenStore());
 
-    @Override
-    public void configure(
-            AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-        oauthServer.tokenKeyAccess("isAnonymous() || hasAuthority('ROLE_TRUSTED_CLIENT')");
-        oauthServer.checkTokenAccess("hasAuthority('ROLE_TRUSTED_CLIENT')");
-        // 如果没有支持allowFormAuthenticationForClients或者有支持但是url中没有client_id和client_secret的，走basic认证保护
-        // https://blog.csdn.net/u012040869/article/details/80140515
-        oauthServer.allowFormAuthenticationForClients();
+	}
 
-    }
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+		oauthServer.tokenKeyAccess("isAnonymous() || hasAuthority('ROLE_TRUSTED_CLIENT')");
+		oauthServer.checkTokenAccess("hasAuthority('ROLE_TRUSTED_CLIENT')");
+		// 如果没有支持allowFormAuthenticationForClients或者有支持但是url中没有client_id和client_secret的，走basic认证保护
+		// https://blog.csdn.net/u012040869/article/details/80140515
+		oauthServer.allowFormAuthenticationForClients();
 
-    @Override
-    public void configure(
-            ClientDetailsServiceConfigurer clients) throws Exception {
+	}
 
-        // clients.inMemory()
-        // .withClient("normal-app")
-        // .authorizedGrantTypes("authorization_code", "implicit")
-        // .authorities("ROLE_CLIENT")
-        // .scopes("read", "write")
-        // .resourceIds(resourceId)
-        // .accessTokenValiditySeconds(accessTokenValiditySeconds)
-        // .and()
-        // .withClient("trusted-app")
-        // .authorizedGrantTypes("client_credentials", "password")
-        // .authorities("ROLE_TRUSTED_CLIENT")
-        // .scopes("read", "write")
-        // .resourceIds(resourceId)
-        // .accessTokenValiditySeconds(accessTokenValiditySeconds)
-        // .secret("secret");
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 
-        // 默认明文，不加密 ，应该改造 加个缓存 不然数据库读取太多！
-        ClientDetailsService clientDetailsService = new JdbcClientDetailsService(dataSource);
+		// 默认明文，不加密 ，应该改造 加个缓存 不然数据库读取太多！
+		ClientDetailsService clientDetailsService = new JdbcClientDetailsService(dataSource);
 
-        clients.withClientDetails(clientDetailsService);
+		clients.withClientDetails(clientDetailsService);
 
-    }
+	}
 
-    /** 其实默认有一个 但是，需要支持刷新token 非必要 **/
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        // 如果不配置此项 则没有RefreshToken 返回
-        defaultTokenServices.setSupportRefreshToken(true);
-        return defaultTokenServices;
-    }
+	/** 其实默认有一个 但是，需要支持刷新token 非必要 **/
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+		DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		defaultTokenServices.setTokenStore(tokenStore());
+		// 如果不配置此项 则没有RefreshToken 返回
+		defaultTokenServices.setSupportRefreshToken(true);
+		return defaultTokenServices;
+	}
 
-    /**
-     * token store
-     *
-     * @param accessTokenConverter
-     * @return
-     */
-    @Bean
-    public TokenStore tokenStore() {
-        TokenStore tokenStore = new JwtTokenStore(accessTokenConverter());
+	/**
+	 * token store
+	 *
+	 * @param accessTokenConverter
+	 * @return
+	 */
+	@Bean
+	public TokenStore tokenStore() {
 
-        return tokenStore;
-    }
+		// TokenStore tokenStore = new JwtTokenStore(accessTokenConverter());
+		TokenStore tokenStore = new RedisTokenStore(connectionFactory);
+		return tokenStore;
+	}
 }
