bd4738e9aa68ff97dbb0c8d39264b5ba488f2e0e
Deament <591518884@qq.com>
2018-10-01 15:58:31 +0800

    no commit message

--- uplus-auth-admin/src/main/java/com/uplus/auth/config/ResourceServerConfiguration.java
+++ uplus-auth-admin/src/main/java/com/uplus/auth/config/ResourceServerConfiguration.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
@@ -20,7 +22,7 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /***
@@ -32,101 +34,101 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
-    private static class OAuthRequestedMatcher implements RequestMatcher {
-        @Override
-        public boolean matches(
-                HttpServletRequest request) {
-            String auth = request.getHeader("Authorization");
-            // Determine if the client request contained an OAuth Authorization
-            boolean haveOauth2Token = (auth != null) && auth.startsWith("Bearer");
-            boolean haveAccessToken = request.getParameter("access_token") != null;
-            return haveOauth2Token || haveAccessToken;
-        }
-    }
+	private static class OAuthRequestedMatcher implements RequestMatcher {
+		@Override
+		public boolean matches(HttpServletRequest request) {
+			String auth = request.getHeader("Authorization");
+			// Determine if the client request contained an OAuth Authorization
+			boolean haveOauth2Token = (auth != null) && auth.startsWith("Bearer");
+			boolean haveAccessToken = request.getParameter("access_token") != null;
+			return haveOauth2Token || haveAccessToken;
+		}
+	}
 
-    @Value("${resource.id:spring-boot-application}")
-    private String resourceId;
+	@Autowired
+	private RedisConnectionFactory connectionFactory;
+	@Value("${resource.id:spring-boot-application}")
+	private String resourceId;
 
-    /**
-     * Token转换器必须与认证服务一致
-     *
-     * @return
-     */
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter() {
-            /***
-             * 重写增强token方法,用于自定义一些token返回的信息
-             */
-            @Override
-            public OAuth2AccessToken enhance(
-                    OAuth2AccessToken accessToken,
-                    OAuth2Authentication authentication) {
-                // String userName = authentication.getUserAuthentication().getName();
-                // 与登录时候放进去的UserDetail实现类一直查看link{SecurityConfiguration}
+	/**
+	 * Token转换器必须与认证服务一致
+	 *
+	 * @return
+	 */
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter() {
+			/***
+			 * 重写增强token方法,用于自定义一些token返回的信息
+			 */
+			@Override
+			public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+				// String userName = authentication.getUserAuthentication().getName();
+				// 与登录时候放进去的UserDetail实现类一直查看link{SecurityConfiguration}
 
-                // /** 自定义一些token属性 ***/
-                final Map<String, Object> additionalInformation = new HashMap<>();
-                //
-                // additionalInformation.put("user",
-                // authentication.getUserAuthentication().getPrincipal());
-                // additionalInformation.put("userName", userName);
-                // additionalInformation.put("roles", user.getAuthorities());
-                ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
-                OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
-                return enhancedToken;
-            }
+				// /** 自定义一些token属性 ***/
+				final Map<String, Object> additionalInformation = new HashMap<>();
+				//
+				// additionalInformation.put("user",
+				// authentication.getUserAuthentication().getPrincipal());
+				// additionalInformation.put("userName", userName);
+				// additionalInformation.put("roles", user.getAuthorities());
+				((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
+				OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
+				return enhancedToken;
+			}
 
-        };
-        accessTokenConverter.setSigningKey("auth");// 测试用,资源服务使用相同的字符达到一个对称加密的效果,生产时候使用RSA非对称加密方式
-        return accessTokenConverter;
-    }
+		};
+		accessTokenConverter.setSigningKey("123");// 测试用,资源服务使用相同的字符达到一个对称加密的效果,生产时候使用RSA非对称加密方式
+		return accessTokenConverter;
+	}
 
-    @Override
-    public void configure(
-            HttpSecurity http) throws Exception {
-        // @formatter:off
-        http.requestMatcher(new OAuthRequestedMatcher()).authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated();
-        // @formatter:on
-    }
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http.requestMatcher(new OAuthRequestedMatcher()).authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll()
+				.anyRequest().authenticated();
+		// @formatter:on
+	}
 
-    //
-    // ===================================================以下代码与认证服务器一致=========================================
+	//
+	// ===================================================以下代码与认证服务器一致=========================================
 
-    @Override
-    public void configure(
-            ResourceServerSecurityConfigurer resources) {
-        // @formatter:off
-        resources.resourceId(resourceId);
-        resources.tokenServices(defaultTokenServices());
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) {
+		// @formatter:off
+		resources.resourceId(resourceId);
+		resources.tokenServices(defaultTokenServices());
 
-        // @formatter:on
-    }
+		// @formatter:on
+	}
 
-    /**
-     * 创建一个默认的资源服务token
-     *
-     * @return
-     */
-    @Bean
-    public ResourceServerTokenServices defaultTokenServices() {
-        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenEnhancer(accessTokenConverter());
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
-    }
-    //
-    // ===================================================以上代码与认证服务器一致=========================================
+	/**
+	 * 创建一个默认的资源服务token
+	 *
+	 * @return
+	 */
+	@Bean
+	public ResourceServerTokenServices defaultTokenServices() {
+		final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+		// defaultTokenServices.setTokenEnhancer(accessTokenConverter());
+		defaultTokenServices.setTokenStore(tokenStore());
+		return defaultTokenServices;
+	}
+	//
+	// ===================================================以上代码与认证服务器一致=========================================
 
-    /**
-     * token存储,这里使用jwt方式存储
-     *
-     * @param accessTokenConverter
-     * @return
-     */
-    @Bean
-    public TokenStore tokenStore() {
-        TokenStore tokenStore = new JwtTokenStore(accessTokenConverter());
-        return tokenStore;
-    }
+	/**
+	 * token存储,这里使用jwt方式存储
+	 *
+	 * @param accessTokenConverter
+	 * @return
+	 */
+	@Bean
+	public TokenStore tokenStore() {
+		// TokenStore tokenStore = new JwtTokenStore(accessTokenConverter());
+		TokenStore tokenStore = new RedisTokenStore(connectionFactory);
+
+		return tokenStore;
+	}
 }
