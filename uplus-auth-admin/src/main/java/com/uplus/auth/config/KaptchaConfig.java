b72baaab1c7ef0e8d93410b7dec7be31da131604
Deament <591518884@qq.com>
2018-11-24 22:47:57 +0800

    更新为springboot2版本 添加验证码的实现

---
+++ uplus-auth-admin/src/main/java/com/uplus/auth/config/KaptchaConfig.java
@@ -0,0 +1,40 @@
+package com.uplus.auth.config;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+
+/***
+ * 验证码配置。
+ *
+ * @author https://gitee.com/YYDeament/88ybg
+ *
+ * @date 2016/10/1
+ */
+// http://blog.csdn.net/u014104286/article/details/70515004 參考
+//https://www.cnblogs.com/java-spring/p/7793149.html
+@Configuration
+public class KaptchaConfig {
+
+	@Bean
+	public DefaultKaptcha getDefaultKaptcha() {
+		DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+		Properties properties = new Properties();
+		properties.setProperty("kaptcha.border", "yes");
+		properties.setProperty("kaptcha.border.color", "105,179,90");
+		properties.setProperty("kaptcha.textproducer.font.color", "blue");
+		properties.setProperty("kaptcha.image.width", "110");
+		properties.setProperty("kaptcha.image.height", "40");
+		properties.setProperty("kaptcha.textproducer.font.size", "30");
+		properties.setProperty("kaptcha.session.key", "code");
+		properties.setProperty("kaptcha.textproducer.char.length", "4");
+		properties.setProperty("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
+		Config config = new Config(properties);
+		defaultKaptcha.setConfig(config);
+		return defaultKaptcha;
+	}
+}
